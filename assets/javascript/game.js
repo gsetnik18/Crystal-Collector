c2101e93d8d1c172c852a216d00ef65a96df9a5e
Gregory Setnik <gsetnik18@gmail.com>
2019-02-17 16:19:35 -0500

    global variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-console.log("Sup Bitch");
+console.log("Sup Bitches");
 //Set global variables for crystal values, wins, losses, target value, user score
 //A game start will store random values of 1-12 in each of the crystals
 //Clicking on a crystal will add that crystal's value to user score
@@ -8,16 +8,22 @@ console.log("Sup Bitch");
 //If win count < loss count && loss count > 5, user loses - alert "You're Fired"
 
 //GLOBAL VARIABLES
+//Crystal values
 var crystal1Value = '0';
 var crystal2Value = '0';
 var crystal3Value = '0';
 var crystal4Value = '0';
+//Score trackers
 var winCount = '0';
 var lossCount = '0';
 var dailyQuota = '0';
 var userScore = '0';
+//game states
 var gameStarted = 'false';
 
-console.log(crystal1Value);
 
 //Game Start
+function reset()
+{
+    
+}
